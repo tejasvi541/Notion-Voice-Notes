0b7549532538abbfed12f70abc267f487cffb7c4
Tejasvi <tejasvi.tanwar541@gmail.com>
2022-05-03 22:14:35 +0530

    Auth update

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,17 @@
+import imp
 from fastapi import APIRouter, Depends, status, HTTPException, Response,Request
+from requests import Request, Session
+import requests as rr
 
 router = APIRouter(tags=["Authentication"])
 
-@router.post("/auth")
-async def auth():
-    return {"message":"Login"}
+notion_client_id = ""
+notion_client_secret = ""
+
+@router.get("/login/{code}")
+async def auth(code:str):
+    print(code)
+    return {"code" : code}
+
+
+    
