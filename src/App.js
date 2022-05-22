76c637cf46a487162f695ae1e63c787011252395
naveen8801 <naveensharma10d@gmail.com>
2022-05-22 19:56:54 +0530

    stylings

--- src/App.js
+++ src/App.js
@@ -14,32 +14,56 @@ function App() {
     resetTranscript,
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
+
   const startListening = () =>
     SpeechRecognition.startListening({ continuous: true });
+
   const [dbs, setdbs] = useState([]);
+  const [user, setUser] = useState(null);
   const params = new URL(window.document.location).searchParams;
+
   useEffect(() => {
-    if (params) {
-      fetchAccessToken();
+    fetchAccessToken();
+    if (
+      window.localStorage.getItem('token') &&
+      window.localStorage.getItem('user')
+    ) {
+      setUser(JSON.parse(localStorage.getItem('user'))?.name);
     }
   }, []);
+  console.log(user);
 
   const fetchAccessToken = async () => {
     const code = params.get('code');
     if (!code) {
       return;
     } else {
-      axios
+      await axios
         .get(`http://127.0.0.1:8000/login/${code}`)
         .then(function (response) {
-          window.localStorage.setItem(
-            'token',
-            response?.data?.Response?.access_token
-          );
+          if (response.data.Response.error) {
+            console.log('i');
+            return;
+          }
+          if (response.data.Response) {
+            window.localStorage.setItem(
+              'token',
+              response?.data?.Response?.access_token
+            );
+            window.localStorage.setItem(
+              'user',
+              JSON.stringify({
+                name: response?.data?.Response?.owner?.user?.name,
+                avatar: response?.data?.Response?.owner?.user?.avatar_id,
+              })
+            );
+            setUser(response?.data?.Response?.owner?.user?.name);
+          }
         })
         .catch(function (error) {
           console.log(error);
         });
+      window.history.replaceState({}, '', `${window.location.pathname}`);
     }
   };
 
@@ -51,22 +75,132 @@ function App() {
     console.log(transcript);
   }, [listening]);
 
+  const logoutHandler = () => {
+    window.localStorage.setItem('token', null);
+    window.localStorage.setItem('user', null);
+    setUser(null);
+  };
+
   return (
     <div className="App">
-      <h2 className="heading"> Studdy Buddy</h2>
-      <a
-        style={{ display: 'block', color: '#FF6363' }}
-        href={`https://api.notion.com/v1/oauth/authorize?owner=user&client_id=8d8bc085-e453-4630-8a3e-03e662398521&redirect_uri=http://localhost:3000/&response_type=code`}
-      >
-        Connect to Notion
-      </a>
-      <div>
-        <p>Microphone: {listening ? 'on' : 'off'}</p>
-        <button onClick={startListening}>Start</button>
-        <button onClick={SpeechRecognition.stopListening}>Stop</button>
-        <button onClick={resetTranscript}>Reset</button>
-        <p>{transcript}</p>
-      </div>
+      {user ? (
+        <>
+          <div className="navbar">
+            <div>
+              <h3 style={{ color: '#FF6363' }} className="nav-item">
+                {' '}
+                Welcome {user}
+              </h3>
+              <h3
+                style={{ color: '#FF6363' }}
+                className="nav-item"
+                onClick={() => logoutHandler()}
+              >
+                {' '}
+                Logout
+              </h3>
+            </div>
+          </div>
+          <h2 className="heading"> Studdy Buddy</h2>
+          <div>
+            <i>
+              <p
+                style={{
+                  color: 'white',
+                }}
+              >
+                Microphone: {listening ? 'on' : 'off'}
+              </p>
+            </i>
+            <button
+              style={{
+                display: 'inline-block',
+                color: 'white',
+                backgroundColor: '#FF5D5D',
+                padding: '0.4rem',
+                borderRadius: '0.4rem',
+                marginTop: '1rem',
+                width: '150px',
+                margin: '0.5rem',
+                fontSize: '18px',
+              }}
+              className="btns"
+              onClick={startListening}
+            >
+              Start
+            </button>
+            <button
+              style={{
+                display: 'inline-block',
+                color: 'white',
+                backgroundColor: '#FF5D5D',
+                padding: '0.4rem',
+                borderRadius: '0.4rem',
+                marginTop: '1rem',
+                width: '150px',
+                margin: '0.5rem',
+                fontSize: '18px',
+              }}
+              className="btns"
+              onClick={SpeechRecognition.stopListening}
+            >
+              Stop
+            </button>
+            <button
+              style={{
+                display: 'inline-block',
+                color: 'white',
+                backgroundColor: '#FF5D5D',
+                padding: '0.4rem',
+                borderRadius: '0.4rem',
+                marginTop: '1rem',
+                width: '150px',
+                margin: '0.5rem',
+                fontSize: '18px',
+              }}
+              className="btns"
+              onClick={resetTranscript}
+            >
+              Reset
+            </button>
+            <i>
+              <p
+                style={{
+                  color: '#FF8C8C',
+                }}
+              >
+                <p>{transcript}</p>
+              </p>
+            </i>
+          </div>
+        </>
+      ) : (
+        <div>
+          <h2 className="heading"> Studdy Buddy</h2>
+          <i>
+            <p
+              style={{
+                color: 'white',
+              }}
+            >
+              You have to sign in with notion to access our application :)
+            </p>
+          </i>
+          <a
+            style={{
+              display: 'inline-block',
+              color: 'white',
+              backgroundColor: '#FF5D5D',
+              padding: '1rem',
+              borderRadius: '0.4rem',
+              marginTop: '1rem',
+            }}
+            href={`https://api.notion.com/v1/oauth/authorize?owner=user&client_id=8d8bc085-e453-4630-8a3e-03e662398521&redirect_uri=http://localhost:3000/&response_type=code`}
+          >
+            Connect to Notion
+          </a>
+        </div>
+      )}
     </div>
   );
 }
