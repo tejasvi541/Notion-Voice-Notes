b999229192333577f66dc8a8c9d227496edb4c60
Tejasvi <tejasvi.tanwar541@gmail.com>
2022-05-22 10:55:48 +0530

    Changed audio save format

--- src/App.jsx
+++ src/App.jsx
@@ -1,111 +1,113 @@
-import { useEffect, useState } from 'react';
-import useRecorder from './utils/useRecorder';
-import './App.css';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import useRecorder from "./utils/useRecorder";
+import "./App.css";
+import axios from "axios";
 
 // The OAuth client ID from the integration page!
-const oauth_client_id = '8d8bc085-e453-4630-8a3e-03e662398521';
+const oauth_client_id = "8d8bc085-e453-4630-8a3e-03e662398521";
 
 function App() {
-  let [audioURL, isRecording, startRecording, stopRecording] = useRecorder();
-  const [dbs, setdbs] = useState([]);
-  const params = new URL(window.document.location).searchParams;
-  // When you open the app, this doesn't do anything, but after you sign into Notion, you'll be redirected back with a code at which point we call our backend.
-  useEffect(() => {
-    if (params) {
-      fetchAccessToken();
-    }
-  }, []);
-  // console.log(new File([audioURL], 'audio.wav'));
+	let [audioURL, isRecording, startRecording, stopRecording] = useRecorder();
+	const [dbs, setdbs] = useState([]);
+	const params = new URL(window.document.location).searchParams;
+	// When you open the app, this doesn't do anything, but after you sign into Notion, you'll be redirected back with a code at which point we call our backend.
+	useEffect(() => {
+		if (params) {
+			fetchAccessToken();
+		}
+	}, []);
+	// console.log(new File([audioURL], 'audio.wav'));
 
-  const sendAudio = async () => {
-    let fd = new FormData();
-    fd.append('file', audioURL);
-    // fd.append('data', audioURL);
-    axios
-      .post(`http://127.0.0.1:8000/speech`, fd)
-      .then(function (response) {
-        window.localStorage.setItem(
-          'token',
-          response?.data?.Response?.access_token
-        );
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+	const sendAudio = async () => {
+		let fd = new FormData();
+		fd.append("file", audioURL);
+		fd.append("data", audioURL);
+		axios
+			.post(`http://127.0.0.1:8000/speech`, fd)
+			.then(function (response) {
+				window.localStorage.setItem(
+					"token",
+					response?.data?.Response?.access_token
+				);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
 
-    // var reader = new window.FileReader();
-    // reader.readAsDataURL(audioURL);
-    // reader.onloadend = function () {
-    //   var fd = new FormData();
-    //   base64data = reader.result;
-    //   fd.append('file', base64data, 'audio.wav');
-    //   axios
-    //     .post(`http://127.0.0.1:8000/speech`, {
-    //       file: fd,
-    //     })
-    //     .then(function (response) {
-    //       window.localStorage.setItem(
-    //         'token',
-    //         response?.data?.Response?.access_token
-    //       );
-    //     })
-    //     .catch(function (error) {
-    //       console.log(error);
-    //     });
-    // };
-  };
+		// console.log(audioURL);
 
-  // console.log(audioURL);
-  const fetchAccessToken = async () => {
-    const code = params.get('code');
-    if (!code) {
-      return;
-    } else {
-      axios
-        .get(`http://127.0.0.1:8000/login/${code}`)
-        .then(function (response) {
-          window.localStorage.setItem(
-            'token',
-            response?.data?.Response?.access_token
-          );
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
-  };
+		// var reader = new window.FileReader();
+		// reader.readAsDataURL(audioURL);
+		// reader.onloadend = function () {
+		//   var fd = new FormData();
+		//   base64data = reader.result;
+		//   fd.append('file', base64data, 'audio.wav');
+		//   axios
+		//     .post(`http://127.0.0.1:8000/speech`, {
+		//       file: fd,
+		//     })
+		//     .then(function (response) {
+		//       window.localStorage.setItem(
+		//         'token',
+		//         response?.data?.Response?.access_token
+		//       );
+		//     })
+		//     .catch(function (error) {
+		//       console.log(error);
+		//     });
+		// };
+	};
 
-  return (
-    <div className="App">
-      <h2 className="heading"> Studdy Buddy</h2>
-      <a
-        style={{ display: 'block', color: '#FF6363' }}
-        href={`https://api.notion.com/v1/oauth/authorize?owner=user&client_id=8d8bc085-e453-4630-8a3e-03e662398521&redirect_uri=http://localhost:3000/&response_type=code`}
-      >
-        Connect to Notion
-      </a>
-      <div>
-        <div className="controllers">
-          <audio src={audioURL} controls />
-          <button
-            className="start_btn"
-            onClick={startRecording}
-            disabled={isRecording}
-          >
-            Start recording
-          </button>
-          <button
-            className="stop_btn"
-            onClick={stopRecording}
-            disabled={!isRecording}
-          >
-            Stop recording
-          </button>
-        </div>
-      </div>
-      <button onClick={() => sendAudio()}> Send Audio</button>
-      {/* {dbs.map((db) => (
+	// console.log(audioURL);
+	const fetchAccessToken = async () => {
+		const code = params.get("code");
+		if (!code) {
+			return;
+		} else {
+			axios
+				.get(`http://127.0.0.1:8000/login/${code}`)
+				.then(function (response) {
+					window.localStorage.setItem(
+						"token",
+						response?.data?.Response?.access_token
+					);
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+		}
+	};
+
+	return (
+		<div className="App">
+			<h2 className="heading"> Studdy Buddy</h2>
+			<a
+				style={{ display: "block", color: "#FF6363" }}
+				href={`https://api.notion.com/v1/oauth/authorize?owner=user&client_id=8d8bc085-e453-4630-8a3e-03e662398521&redirect_uri=http://localhost:3000/&response_type=code`}
+			>
+				Connect to Notion
+			</a>
+			<div>
+				<div className="controllers">
+					<audio src={audioURL} controls />
+					<button
+						className="start_btn"
+						onClick={startRecording}
+						disabled={isRecording}
+					>
+						Start recording
+					</button>
+					<button
+						className="stop_btn"
+						onClick={stopRecording}
+						disabled={!isRecording}
+					>
+						Stop recording
+					</button>
+				</div>
+			</div>
+			<button onClick={() => sendAudio()}> Send Audio</button>
+			{/* {dbs.map((db) => (
 				<div
 					style={{
 						display: "inline-flex",
@@ -117,8 +119,8 @@ function App() {
 					{JSON.stringify(db, null, 2)}
 				</div>
 			))} */}
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
--- src/utils/useRecorder.jsx
+++ src/utils/useRecorder.jsx
@@ -1,50 +1,59 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const useRecorder = () => {
-  const [audioURL, setAudioURL] = useState('');
-  const [isRecording, setIsRecording] = useState(false);
-  const [recorder, setRecorder] = useState(null);
+	const [audioURL, setAudioURL] = useState("");
+	const [isRecording, setIsRecording] = useState(false);
+	const [recorder, setRecorder] = useState(null);
 
-  useEffect(() => {
-    // Lazily obtain recorder first time we're recording.
-    if (recorder === null) {
-      if (isRecording) {
-        requestRecorder().then(setRecorder, console.error);
-      }
-      return;
-    }
+	useEffect(() => {
+		// Lazily obtain recorder first time we're recording.
+		if (recorder === null) {
+			if (isRecording) {
+				requestRecorder().then(setRecorder, console.error);
+			}
+			return;
+		}
 
-    // Manage recorder state.
-    if (isRecording) {
-      recorder.start();
-    } else {
-      recorder.stop();
-    }
+		// Manage recorder state.
+		if (isRecording) {
+			recorder.start();
+		} else {
+			recorder.stop();
+		}
 
-    // Obtain the audio when ready.
-    const handleData = (e) => {
-      console.log(e.data);
-      // setAudioURL(URL.createObjectURL(e.data));
-      setAudioURL(e.data);
-    };
+		// Obtain the audio when ready.
+		const handleData = (e) => {
+			console.log(e.data);
+			// setAudioURL(URL.createObjectURL(e.data));
+			const blobDataInWavFormat = new Blob([e.data], {
+				type: "audio/wav; codecs=MS_PCM",
+			});
+			setAudioURL(blobDataInWavFormat);
+		};
 
-    recorder.addEventListener('dataavailable', handleData);
-    return () => recorder.removeEventListener('dataavailable', handleData);
-  }, [recorder, isRecording]);
+		recorder.addEventListener("dataavailable", handleData);
+		return () => recorder.removeEventListener("dataavailable", handleData);
+	}, [recorder, isRecording]);
 
-  const startRecording = () => {
-    setIsRecording(true);
-  };
+	const startRecording = () => {
+		setIsRecording(true);
+	};
 
-  const stopRecording = () => {
-    setIsRecording(false);
-  };
+	const stopRecording = () => {
+		setIsRecording(false);
+	};
 
-  return [audioURL, isRecording, startRecording, stopRecording];
+	return [audioURL, isRecording, startRecording, stopRecording];
 };
 
 async function requestRecorder() {
-  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-  return new MediaRecorder(stream);
+	const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+	const mime = ["audio/wav", "audio/mpeg", "audio/webm", "audio/ogg"].filter(
+		MediaRecorder.isTypeSupported
+	)[0];
+	const recorder = new MediaRecorder(stream, {
+		mimeType: mime,
+	});
+	return recorder;
 }
 export default useRecorder;
