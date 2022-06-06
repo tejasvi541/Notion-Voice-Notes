5510c4f6f392e51e8781e6af7701a68c52de473e
naveen8801 <naveensharma10d@gmail.com>
2022-06-06 14:00:47 +0530

    refactor

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const rateLimit = require("express-rate-limit");
-const mongoSanitize = require("express-mongo-sanitize");
-const xssClean = require("xss-clean");
-const cors = require("cors");
-const morgan = require("morgan");
-const connectDB = require("./config/db");
-const cookieParser = require("cookie-parser");
-const path = require("path");
-const colors = require("colors");
+const express = require('express');
+const dotenv = require('dotenv');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xssClean = require('xss-clean');
+const cors = require('cors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const colors = require('colors');
 
 // Load env vars
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
-connectDB();
+// connectDB();
 
 // Initialise app
 
@@ -26,20 +26,20 @@ app.use(express.json);
 app.use(cookieParser());
 
 // Development logging middleware
-if (process.env.NODE_ENV === "development") {
-	app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 // Sanitize data
-app.use(mongoSanitize());
+// app.use(mongoSanitize());
 
 // Cross site scripting attacks
-app.use(xssClean());
+// app.use(xssClean());
 
 // Rate limiting
 const limiter = rateLimit({
-	windowMs: 10 * 60 * 1000,
-	max: 1000,
+  windowMs: 10 * 60 * 1000,
+  max: 1000,
 });
 
 app.use(limiter);
@@ -48,19 +48,26 @@ app.use(limiter);
 app.use(cors());
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(
-	PORT,
-	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-	)
-);
-
-// Handel unhandelled promise rejections
-process.on("unhandledRejection", (err) => {
-	console.log(`Error : ${err.message}`.red);
-	// Close Server & Exit process
-	server.close(() => process.exit(1));
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
+// const server = app.listen(
+//   PORT,
+//   console.log(
+//     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+//   )
+// );
+
+// // Handel unhandelled promise rejections
+// process.on('unhandledRejection', (err) => {
+//   console.log(`Error : ${err.message}`.red);
+//   // Close Server & Exit process
+//   server.close(() => process.exit(1));
+// });
